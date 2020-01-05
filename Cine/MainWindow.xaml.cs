6bcffba21d0097259f34b4a80ef6ebedcbc50fcb
Nicole Zambrano <znicole473@gmail.com>
2020-01-05 12:37:29 -0500

    Conexión base de datos Login Actualización base

---
+++ Cine/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MediaButton.Content == FindResource("Play"))
+            {
+                MediaButton.Content = FindResource("Stop");
+            }
+            else
+            {
+                MediaButton.Content = FindResource("Play");
+            }
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private bool _isPlaying = false;
+        private RelayCommand _playCommand;
+
+        public ViewModel()
+        {
+            isPlaying = false;
+        }
+
+        public bool isPlaying
+        {
+            get { return _isPlaying; }
+            set
+            {
+                _isPlaying = value;
+                OnPropertyChanged("isPlaying");
+            }
+        }
+
+        public ICommand PlayCommand
+        {
+            get
+            {
+                return _playCommand ?? new RelayCommand((x) =>
+                {
+                    var buttonType = x.ToString();
+
+                    if (null != buttonType)
+                    {
+                        if (buttonType.Contains("asientoSeleccionado"))
+                        {
+                            isPlaying = false;
+                        }
+                        else if (buttonType.Contains("asientoDeseleccionado"))
+                        {
+                            isPlaying = true;
+                        }
+                    }
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+
+            if (_canExecute == null)
+            {
+                return true;
+            }
+
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+    }
+
+}
+
+
+
---
+++ Cine/WndLogin.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cine
+{
+    /// <summary>
+    /// Lógica de interacción para WndLogin.xaml
+    /// </summary>
+    public partial class WndLogin : Window
+    { 
+        public WndLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void InicioSesion(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=FZAMBRANO-OPER;Initial Catalog=cine;Integrated Security=True");
+
+            try
+            {
+                if (sqlcon.State == ConnectionState.Closed)
+                    sqlcon.Open();
+                String query = "SELECT COUNT(1) FROM Usuario WHERE Nom_usuario=@correo AND pwd_usuario=@pwd";
+                SqlCommand sqlcmd = new SqlCommand(query, sqlcon);
+                sqlcmd.CommandType = CommandType.Text;
+                sqlcmd.Parameters.AddWithValue("@correo", textEmail.Text);
+                sqlcmd.Parameters.AddWithValue("@pwd", textPwd.Text);
+                int count = Convert.ToInt32(sqlcmd.ExecuteScalar());
+
+                if(count == 1)
+                {
+                    PeliculasWindows loginS = new PeliculasWindows();
+                    loginS.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos");
+                }
+
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                sqlcon.Close();
+            }
+
+            
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
+        
